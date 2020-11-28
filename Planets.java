94e9d453f0d8a8b8b4fb6e09e2820fa5be9e9895
Seth Warren <sethajwarren@gmail.com>
2020-11-28 10:45:55 -0600

    Made some changes and expanded the classes more. Idk if it is efficient.

--- Planets.java
+++ Planets.java
@@ -1,28 +1,37 @@
 /********************************************************
-* This file holds Planet class and the attibutes and
-* behavior of the planets in the game.
+* This file holds the Planet class and the differnt
+* planet classes along with the attibutes and behavior 
+* of the planets in the game.
 ********************************************************/
+
+/*
+Planet Biome Key:
+	0 = forest
+	1 = city/metropolitan
+	2 = snow
+	3 = desert
+	4 = ocean
+	5 = lava
+		
+Planet Size Scale
+	0 = moon
+	1 = small
+	2 = med
+	3 = large
+
+Planet weather key:
+	0 = clear
+	1 = rain
+	2 = fog
+	3 = snow
+*/
 
 class Planet
 {
-	/*
-	Planet Biome Key:
-		0 = forest
-		1 = city/metropolitan
-		2 = snow
-		3 = desert
-		4 = ocean
-		5 = lava
-		
-	Planet Size Scale
-		0 = moon
-		1 = small
-		2 = med
-		3 = large
-	*/
 	
 	private int size;
 	private int biome;
+	private int weather;
 	
 	void Planet()
 	{
@@ -48,6 +57,45 @@ class Planet
 	int getBiome()
 	{
 		return biome;
+	}
+
+	void setWeather (int data)
+	{
+		weather = data;
+	}
+	
+	int getWeather()
+	{
+		return weather;
+	}
+}
+
+class System
+{
+	private char control;
+	private int temp;
+	private Planet planet;
+	
+	void Tattoine()
+	{
+		control = 'N'
+		temp = 50;
+		planet = new Planet();
+	}
+	
+	void setCont(char data)
+	{
+		control = data;
+	}
+	
+	char getCont()
+	{
+		return control;
+	}
+	
+	void setTemp(int data)
+	{
+		temp = data;
 	}
 }
 		
--- Troops.java
+++ Troops.java
@@ -3,25 +3,34 @@
 * behavior of the planets in the game.
 ********************************************************/
 
-class Troop
-{
-	/*
-	Troop Type Key:
-		0 = Untrained
-		1 = Infantry
-		2 = Officer
-		3 = Small Vehicle
-		4 = Large Vehicle
+/*
+Unit Type Key:
+	0 = Untrained
+	1 = Infantry
+	2 = Officer
+	3 = Small Vehicle
+	4 = Large Vehicle
 		
-	Troop Weapon Key:
-		0 = melee
-		1 = small arms
-		2 = heavy arms
-		3 = explosive
-	*/
+Unit Weapon Key:
+	0 = melee
+	1 = small arms
+	2 = heavy arms
+	3 = artillery
+	4 = explosive
+	
+Unit training key:
+	0 = untrained
+	1 = trained
+	2 = skilled
+	3 = specialist
+*/
+
+class Unit
+{
 	
 	private int type;
 	private int weap;
+	private int train;
 	
 	void Troop()
 	{
@@ -48,4 +57,59 @@ class Troop
 	{
 		return weap;
 	}
+
+	void setTrain(int data)
+	{
+		train = data;
+	}
 	
+	int getTrain()
+	{
+		return train;
+	}
+}
+
+class Trooper
+{
+	private Unit trooper;
+	
+	void Trooper()
+	{
+		trooper = new Unit();
+		
+		trooper.setTrain(0);
+		trooper.setType(1);
+		trooper.setWeap(1);
+	}
+}
+
+class Tank
+{
+	private Unit tank;
+	
+	void Tank()
+	{
+		tank = new Unit();
+		
+		tank.setTrain(0);
+		tank.setType(3);
+		tank.setWeap(4);
+	}
+}
+
+class Walker
+{
+	
+	private Unit walker;
+	
+	
+	void Walker()
+	{
+		walker = new Walker();
+		
+		walker.setTrain(0);
+		walker.setType(4);
+		walker.setWeap(3);
+	}
+}
+

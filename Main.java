bb0d28e8fd3bcf87796b3fb8f3ae034668d40fa2
Seth Warren <sethajwarren@gmail.com>
2020-12-02 16:36:04 -0600

    Here we go

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*************************************************
+* This file will be the file where the main part
+* of the game takes place
+*************************************************/
+
+class Main
+{
+	public static void main(String[] args)
+	{
+		//do stuff
+	}
+}
